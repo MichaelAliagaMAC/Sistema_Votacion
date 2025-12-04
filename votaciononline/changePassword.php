d4ff1c8f3c282e81b20272422c1c0d2c4eb46df6
Michael <139723708+michael1997070397@users.noreply.github.com>
2025-12-03 23:56:50 -0500

    Actualización de indez.html

--- votaciononline/changePassword.php
+++ votaciononline/changePassword.php
@@ -1,101 +1,115 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Cambio de Contraseña - Voto Seguro</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&family=Raleway&family=Oswald&family=Roboto+Condensed&display=swap" rel="stylesheet" />
 
     <style>
-      .headerFont{
+      body {
         font-family: 'Ubuntu', sans-serif;
-        font-size: 24px;
+        background-color: #f4f6f9;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
       }
 
-      .subFont{
-        font-family: 'Raleway', sans-serif;
-        font-size: 14px;
-        
+      .navbar {
+        background-color: #2c3e50;
+        border: none;
       }
-      
-      .specialHead{
+
+      .navbar-brand {
+        color: #ecf0f1 !important;
+        font-weight: bold;
+      }
+
+      .admin-btn {
+        background-color: #27ae60;
+        color: white;
+        border-radius: 20px;
+        padding: 6px 20px;
+        margin-top: 8px;
+      }
+
+      .form-container {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-top: 80px;
+        padding-bottom: 40px;
+      }
+
+      .form-box {
+        background-color: white;
+        border-radius: 10px;
+        box-shadow: 0 0 15px rgba(0,0,0,0.1);
+        padding: 40px;
+        width: 100%;
+        max-width: 400px;
+      }
+
+      .form-box h2 {
         font-family: 'Oswald', sans-serif;
+        font-weight: bold;
+        color: #2c3e50;
+        margin-bottom: 30px;
+        text-align: center;
       }
 
-      .normalFont{
-        font-family: 'Roboto Condensed', sans-serif;
+      .form-control {
+        border-radius: 8px;
+        margin-bottom: 15px;
+      }
+
+      .btn-primary {
+        border-radius: 20px;
+        font-weight: bold;
       }
     </style>
-
   </head>
   <body>
-	
-	<div class="container">
-  	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse
-    " role="navigation">
+    <nav class="navbar navbar-fixed-top">
       <div class="container">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <div class="navbar-header">
-          <a href="cpanel.php" class="navbar-brand headerFont text-lg"><strong>Voto Seguro</strong></a>
+          <a href="cpanel.php" class="navbar-brand">Voto Seguro</a>
         </div>
-
-        <div class="collapse navbar-collapse" id="example-nav-collapse">
-          <ul class="nav navbar-nav">
-
-          </ul>
-          
-
-          <a href="cpanel.php"><button type="submit" class="btn btn-success navbar-right navbar-btn"><span class="normalFont"><strong>Panel Administrativo</strong></span></button></a>
+        <div class="navbar-right">
+          <a href="cpanel.php">
+            <button type="button" class="btn admin-btn navbar-btn">
+              Panel Administrativo
+            </button>
+          </a>
         </div>
-
-      </div> <!-- end of container -->
+      </div>
     </nav>
 
-    
-    <div class="container" style="padding-top:100px;">
-    	<div class="row">
-    		<div class="col-sm-4"></div>
-    		<div class="col-sm-4" style="border:2px solid gray;padding:50px;">
-    			
-    			<div class="page-header">
-    				<h2 class="specialHead">Contraseña de Administrador</h2>
-    			</div>
-          
-          <form action="updatePwd.php" method="POST">
-      			<div class="form-group">
-      				<label for="">Contraseña Actual</label><br>
-      				<input type="text" name="existingPassword" placeholder="Ingrese su actual contraseña" class="form-control"><br>
-
-      				<label for="">Nueva Contraseña</label><br>
-      				<input type="password" name="newPassword" class="form-control" placeholder="Ingrese su nueva contraseña"><br>
-
-              <label for="">Repita la Contraseña</label><br>
-              <input type="password" name="retypePassword" class="form-control" placeholder="Ingrese su contraseña otra vez"><br>
-
-      				<button type="submit" class="btn btn-block span btn-primary "> <span class="glyphicon glyphicon-ok"></span> Cambiar Contraseña</button>
-      			</div>
-          </form>
-    		</div>
-    		<div class="col-sm-4"></div>
-    	</div>
-    </div>
-
+    <div class="form-container">
+      <div class="form-box">
+        <h2>Contraseña de Administrador</h2>
+        <form action="updatePwd.php" method="POST">
+          <div class="form-group">
+            <label>Contraseña Actual</label>
+            <input type="text" name="existingPassword" class="form-control" placeholder="Ingrese su actual contraseña" required />
+            <label>Nueva Contraseña</label>
+            <input type="password" name="newPassword" class="form-control" placeholder="Ingrese su nueva contraseña" required />
+            <label>Repita la Contraseña</label>
+            <input type="password" name="retypePassword" class="form-control" placeholder="Ingrese su contraseña otra vez" required />
+            <button type="submit" class="btn btn-primary btn-block">
+              <span class="glyphicon glyphicon-ok"></span> Cambiar Contraseña
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
--- votaciononline/saveVote.php
+++ votaciononline/saveVote.php
@@ -1,138 +1,135 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-    <title>Bootstrap 101 Template</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Voto Satisfactorio - Voto Seguro</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&family=Raleway&family=Oswald&family=Roboto+Condensed&display=swap" rel="stylesheet" />
 
     <style>
-      .headerFont{
+      body {
         font-family: 'Ubuntu', sans-serif;
-        font-size: 24px;
+        background-color: #f4f6f9;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
       }
 
-      .subFont{
-        font-family: 'Raleway', sans-serif;
-        font-size: 14px;
-        
+      .navbar {
+        background-color: #2c3e50;
+        border: none;
       }
-      
-      .specialHead{
+
+      .navbar-brand {
+        color: #ecf0f1 !important;
+        font-weight: bold;
+      }
+
+      .confirmation-container {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 60px 20px;
+      }
+
+      .confirmation-box {
+        background-color: white;
+        border-radius: 12px;
+        box-shadow: 0 0 20px rgba(0,0,0,0.1);
+        padding: 50px;
+        text-align: center;
+        max-width: 500px;
+        width: 100%;
+      }
+
+      .confirmation-box img {
+        width: 100px;
+        height: 100px;
+        margin-bottom: 30px;
+      }
+
+      .confirmation-box h3 {
         font-family: 'Oswald', sans-serif;
+        font-size: 26px;
+        color: #2c3e50;
+        margin-bottom: 30px;
       }
 
-      .normalFont{
-        font-family: 'Roboto Condensed', sans-serif;
+      .btn-finalizar {
+        background-color: #3498db;
+        color: white;
+        font-weight: bold;
+        border-radius: 30px;
+        padding: 12px 40px;
+        font-size: 18px;
+        box-shadow: 0 4px 10px rgba(0,0,0,0.1);
+        transition: all 0.3s ease;
+        text-decoration: none;
+        display: inline-block;
+      }
+
+      .btn-finalizar:hover {
+        background-color: #2980b9;
+        transform: scale(1.05);
       }
     </style>
-
   </head>
   <body>
-	
-	<div class="container">
-  	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse
-    " role="navigation">
+    <nav class="navbar navbar-fixed-top">
       <div class="container">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <div class="navbar-header">
-          <a href="index.html" class="navbar-brand headerFont text-lg"><strong>Voto Seguro</strong></a>
+          <a href="index.html" class="navbar-brand">Voto Seguro</a>
         </div>
-
-        <div class="collapse navbar-collapse" id="example-nav-collapse">
-          <ul class="nav navbar-nav">
-    
-          </ul>
-          
-
-          <button type="submit" class="btn btn-success navbar-right navbar-btn"><span class="normalFont"><strong>Administrador</strong></span></button>
-        </div>
-
-      </div> <!-- end of container -->
+      </div>
     </nav>
 
-    
-    <div class="container" style="padding-top:150px;">
-    	<div class="row">
-    		<div class="col-sm-4"></div>
-    		<div class="col-sm-4 text-center" style="border:2px solid gray;padding:50px;">
-    			
-    			<?php
-
+    <div class="confirmation-container">
+      <div class="confirmation-box">
+        <?php
           require('config.php');
 
-    							
-				
-          
+          if (isset($_POST["submit"])) {
+            if (isset($_POST["voterName"]) && isset($_POST["voterEmail"]) && isset($_POST["voterID"]) && isset($_POST["selectedCandidate"])) {
+              $name = test_input($_POST["voterName"]);
+              $email = test_input($_POST["voterEmail"]);
+              $voterID = test_input($_POST["voterID"]);
+              $selection = test_input($_POST["selectedCandidate"]);
+            }
+          } else {
+            echo "<br>Todos los campos son obligatorios.";
+          }
 
-					if(isset($_POST["submit"])){
-					if(isset($_POST["voterName"]) && isset($_POST["voterEmail"]) && isset($_POST["voterID"]) && isset($_POST["selectedCandidate"]))
-					{
-						$name= test_input($_POST["voterName"]);
-						$email= test_input($_POST["voterEmail"]);
-						$voterID= test_input($_POST["voterID"]);
-						$selection= test_input($_POST["selectedCandidate"]);
-					}
-				}
-				else
-				{
-					echo "<br>All Field Recquired";
-				}
-				
-       $DB_HOST= "localhost";
-       $DB_USER="root";
-       $DB_PASSWORD="";
-       $DB_NAME="db_evoting";
-	
+          $DB_HOST = "localhost";
+          $DB_USER = "root";
+          $DB_PASSWORD = "";
+          $DB_NAME = "db_evoting";
 
-        $conn= @mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME)
-        or die("Couldn't Connect to Database :");
-				
+          $conn = @mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME) or die("No se pudo conectar a la base de datos.");
 
+          $sql = "INSERT INTO db_evoting.tbl_users VALUES(null,'$name','$email','$voterID','$selection');";
 
-				$sql= "INSERT INTO db_evoting.tbl_users VALUES(null,'".$name."','".$email."','".$voterID."','".$selection."');";
-					
-
-				if(mysqli_query($conn, $sql)){
-					echo "<img src='images/voto_satisfactorio.png' width='70' height='70'>";
-					echo "<h3 class='text-info specialHead text-center'><strong> Has votado satisfactoriamente.</strong></h3>";
-					echo "<a href='index.html' class='btn btn-primary'> <span class='glyphicon glyphicon-ok'></span> <strong> Finalizar</strong> </a>";
-				}
-				else
-				{
-					echo "<img src='images/error.png' width='70' height='70'>";
-					echo "<h3 class='text-info specialHead text-center'><strong> Parece que tenemos un problema.</strong></h3>";
-					echo "<a href='index.html' class='btn btn-primary'> <span class='glyphicon glyphicon-ok'></span> <strong> Finalizar</strong> </a>";
-				}
-
-				
-				?>
-
-				
-    			
-    		</div>
-    		<div class="col-sm-4"></div>
-    	</div>
-    </div>
-
+          if (mysqli_query($conn, $sql)) {
+            echo "<img src='images/voto_satisfactorio.png' alt='Voto exitoso'>";
+            echo "<h3>Has votado satisfactoriamente.</h3>";
+            echo "<a href='index.html' class='btn-finalizar'><span class='glyphicon glyphicon-ok'></span> Finalizar</a>";
+          } else {
+            echo "<img src='images/error.png' alt='Error'>";
+            echo "<h3>Parece que tenemos un problema.</h3>";
+            echo "<a href='index.html' class='btn-finalizar'><span class='glyphicon glyphicon-ok'></span> Finalizar</a>";
+          }
+        ?>
+      </div>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
